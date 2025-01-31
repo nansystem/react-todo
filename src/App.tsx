6aaa990235d3225628997ab16dca7deaf02f4117
nansystem <sato171786@gmail.com>
2025-01-31 13:41:56 +0900

    feat: タスク一覧を表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { TodoList } from './components/TodoList';
+import { dummyTodoList } from './data/dummyTodoList';
+
 function App() {
   return (
     <main className="mx-auto mt-10 max-w-xl">
       <h1 className="text-center text-4xl">Todoアプリ</h1>
+      <div className="rounded bg-slate-200 p-5">
+        <TodoList todoList={dummyTodoList} />
+      </div>
     </main>
   );
 }
---
+++ src/data/dummyTodoList.ts
@@ -0,0 +1,14 @@
+import { Todo } from '../types/todo';
+
+export const dummyTodoList: Todo[] = [
+  { id: '1', title: 'タスク1', completed: false },
+  { id: '2', title: 'タスク2', completed: true },
+  { id: '3', title: 'タスク3', completed: false },
+  { id: '4', title: 'タスク4', completed: true },
+  { id: '5', title: 'タスク5', completed: false },
+  { id: '6', title: 'タスク6', completed: true },
+  { id: '7', title: 'タスク7', completed: false },
+  { id: '8', title: 'タスク8', completed: true },
+  { id: '9', title: 'タスク9', completed: false },
+  { id: '10', title: 'タスク10', completed: true },
+];
